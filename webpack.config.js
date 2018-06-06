030172c3f43674445d1ee639821649687c18b706
Maksymilian Hnatejko <maksymilian.hnatejko@gmail.com>
2018-06-06 23:29:15 +0200

    Setup devDependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = (env) => {
     return {
         entry: './src/app.js', // './src/playground/hoc.js', //, // './src/playground/redux-expensify.js', //
         output: {
-            path: path.join(__dirname, 'public'),
+            path: path.join(__dirname, 'public', 'dist'),
             filename: 'bundle.js'
         },
         module: {
@@ -49,7 +49,8 @@ module.exports = (env) => {
         devtool: isProduction ? 'source-map' : 'inline-source-map',//'cheap-module-eval-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true
+            historyApiFallback: true,
+            publicPath: '/dist/'
         }
         }
 }
